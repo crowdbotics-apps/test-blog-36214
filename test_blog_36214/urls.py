297f556a49052cf5079f2ac82e15a7b8aa9836b7
Crowdbotics <team@crowdbotics.com>
2022-07-16 12:50:29 +0000

    Update models

--- test_blog_36214/urls.py
+++ test_blog_36214/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
     path("rest-auth/registration/account-confirm-email/<str:key>/", confirm_email),
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
     path("api/v1/", include("posts.api.v1.urls")),
+    path("posts/", include("posts.urls")),
 ]
 
 admin.site.site_header = "test_blog"
